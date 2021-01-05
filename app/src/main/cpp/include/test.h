8c47f7cd75bb5692ace4e4804a3e668938267645
archurtan <archurspace@gmail.com>
2021-01-05 19:38:27 +0800

    生成.a 静态库

---
+++ app/src/main/cpp/include/test.h
@@ -0,0 +1,10 @@
+//
+// Created by archur on 1/5/21.
+//
+
+#ifndef TESTJNI_TEST_H
+#define TESTJNI_TEST_H
+
+int add(int a , int b);
+
+#endif //TESTJNI_TEST_H
---
+++ app/src/main/cpp/test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by archur on 1/5/21.
+//
+
+#include <jni.h>
+#include "include/test.h"
+
+int add(int a , int b){
+    return a + b;
+}
